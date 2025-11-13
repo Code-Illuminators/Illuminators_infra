9069fc7d92d71a17bb2114a4b54dc8ee1f90f67c
RostyslavHnatyshyn <rostykgnatyshyn@gmail.com>
2025-11-13 23:04:54 +0200

    add security parameters for tf state bucket

--- account-setup/terrafrom_state/s3.tf
+++ account-setup/terrafrom_state/s3.tf
@@ -4,3 +4,15 @@ resource "aws_s3_bucket" "tf_state" {
     prevent_destroy = true
   }
 }
+
+
+resource "aws_s3_bucket_public_access_block" "tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
