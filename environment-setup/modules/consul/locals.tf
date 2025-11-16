797e8eff9ab54ee22d4ff203ec4f1f12e6c96ecf
oalexyeyev <alexyeyev@gmail.com>
2025-11-16 21:36:08 +0200

    CI-73: fix path to certs

--- environment-setup/modules/consul/locals.tf
+++ environment-setup/modules/consul/locals.tf
@@ -1,9 +1,9 @@
 locals {
   consul_secrets_files = {
-    "consul-agent-ca"          = "certs/consul-agent-ca.pem"
-    "consul-agent-ca-key"      = "certs/consul-agent-ca-key.pem"
-    "dc1-server-consul-0"      = "certs/dc1-server-consul-0.pem"
-    "dc1-server-consul-0-key"  = "certs/dc1-server-consul-0-key.pem"
-    "server-hcl"               = "server.hcl"
+    "consul-agent-ca"          = "/modules/consul/certs/consul-agent-ca.pem"
+    "consul-agent-ca-key"      = "/modules/consul/certs/consul-agent-ca-key.pem"
+    "dc1-server-consul-0"      = "/modules/consul/certs/dc1-server-consul-0.pem"
+    "dc1-server-consul-0-key"  = "/modules/consul/certs/dc1-server-consul-0-key.pem"
+    "server-hcl"               = "/modules/consul/server.hcl"
   }
 }
