d6f9ba0ea9e7d3abb45ced3e8201d8c083677818
fariyonychka <yustyna1409a@gmail.com>
2025-11-19 22:23:38 +0200

    CI-77 Added eks module

--- environment-setup/modules.tf
+++ environment-setup/modules.tf
@@ -28,3 +28,20 @@ module "s3-photo-bucket" {
   source = "./modules/s3-photo-bucket"
   env    = var.env
 }
+
+module "eks-setup" {
+  source                 = "./modules/eks/"
+  vpc-id                 = data.aws_vpc.account-vpc.id
+  env                    = var.env
+  region                 = var.region
+  private-eks-subnet-a   = var.private-eks-subnet-a
+  private-eks-subnet-b   = var.private-eks-subnet-b
+  public-eks-subnet-a    = var.public-eks-subnet-a
+  public-eks-subnet-b    = var.public-eks-subnet-b
+  az-a                   = var.az-a
+  az-b                   = var.az-b
+  cluster-name           = var.cluster-name
+  eks-k8s-version        = var.eks-k8s-version
+  private-route-table-id = module.environment-vpc.private-route-table-id
+  public-route-table-id  = module.environment-vpc.public-route-table-id
+}
---
+++ environment-setup/modules/eks/eks-subnets.tf
@@ -0,0 +1,41 @@
+resource "aws_subnet" "private-eks-subnet-a" {
+  vpc_id            = var.vpc-id
+  cidr_block        = var.private-eks-subnet-a
+  availability_zone = var.az-a
+  tags = merge(var.common_tags_illuminati, {
+    "Name"                                     = "private-eks-subnet-a-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+  })
+}
+
+resource "aws_subnet" "private-eks-subnet-b" {
+  vpc_id            = var.vpc-id
+  cidr_block        = var.private-eks-subnet-b
+  availability_zone = var.az-b
+  tags = merge(var.common_tags_illuminati, {
+    "Name"                                     = "private-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+  })
+}
+
+resource "aws_subnet" "public-eks-subnet-a" {
+  vpc_id                  = var.vpc-id
+  cidr_block              = var.public-eks-subnet-a
+  availability_zone       = var.az-a
+  map_public_ip_on_launch = true
+  tags = merge(var.common_tags_illuminati, {
+    "Name"                                     = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+  })
+}
+
+resource "aws_subnet" "public-eks-subnet-b" {
+  vpc_id                  = var.vpc-id
+  cidr_block              = var.public-eks-subnet-b
+  availability_zone       = var.az-b
+  map_public_ip_on_launch = true
+  tags = merge(var.common_tags_illuminati, {
+    "Name"                                     = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+  })
+}
