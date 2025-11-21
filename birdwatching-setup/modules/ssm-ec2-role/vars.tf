18e78ffaf9bcf6b21c4a3707850f2d820d7c77a0
Oleksandr Vasylovych <vasilovich.aleksandr@gmail.com>
2025-11-21 15:39:35 +0200

    CI-131 add sonnarqube requirments and comments

--- birdwatching-setup/modules/ssm-ec2-role/vars.tf
+++ birdwatching-setup/modules/ssm-ec2-role/vars.tf
@@ -1,4 +1,5 @@
 variable "env" {
+  description = "Specifies the target environment (e.g., dev, stage, prod) for resource provisioning"  
   type = string
 }
 
--- tools-setup/modules/jenkins/jenkins-setup.tf
+++ tools-setup/modules/jenkins/jenkins-setup.tf
@@ -82,6 +82,40 @@ resource "aws_s3_bucket" "ssm_bucket" {
   bucket = "birdwatching-ssm-${var.env}"
 }
 
+resource "aws_s3_bucket_public_access_block" "ssm-bucket-public-access-block" {
+  bucket = aws_s3_bucket.ssm_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "ssm_bucket_https_only" {
+  bucket = aws_s3_bucket.ssm_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "DenyInsecureTransport"
+        Effect = "Deny"
+        Principal = "*"
+        Action = "s3:*"
+        Resource = [
+          aws_s3_bucket.ssm_bucket.arn,
+          "${aws_s3_bucket.ssm_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "ssm_bucket_access" {
   name        = "ssm-bucket-access-${var.env}"
   description = "Access for SSM session temp files"
@@ -103,6 +137,42 @@ resource "aws_iam_policy" "ssm_bucket_access" {
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_logging" "ssm_bucket_logging" {
+  bucket = aws_s3_bucket.ssm_bucket.id
+
+  target_bucket = aws_s3_bucket.ssm_logs_bucket.id
+  target_prefix = "ssm/"
+}
+
+resource "aws_s3_bucket" "ssm_logs_bucket" {
+  bucket = "birdwatching-ssm-logs-${var.env}"
+
+  tags = var.common_tags
+}
+
+data "aws_iam_policy_document" "ssm_logs_bucket" {
+  statement {
+    sid     = "s3-log-delivery"
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["logging.s3.amazonaws.com"]
+    }
+
+    actions = ["s3:PutObject"]
+
+    resources = [
+      "${aws_s3_bucket.ssm_logs_bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "ssm_logs_bucket_policy" {
+  bucket = aws_s3_bucket.ssm_logs_bucket.id
+  policy = data.aws_iam_policy_document.ssm_logs_bucket.json
 }
 
 resource "aws_iam_role_policy_attachment" "jenkins_ssm_bucket_access" {
