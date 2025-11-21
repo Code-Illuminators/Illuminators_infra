d92be1965f33864741b5a05b69b716ec02501304
RostyslavHnatyshyn <rostykgnatyshyn@gmail.com>
2025-11-21 02:05:30 +0200

    change provider configuration

--- environment-setup/main.tf
+++ environment-setup/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.region
-  assume_role {
-    role_arn = "arn:aws:iam::${var.aws_account_id}:role/terraform-deployment-role-${var.env}"
-  }
+  # assume_role {
+  #   role_arn = "arn:aws:iam::${var.aws_account_id}:role/terraform-deployment-role-${var.env}"
+  # }
 }
 
 terraform {
