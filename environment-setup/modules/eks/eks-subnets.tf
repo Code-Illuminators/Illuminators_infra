6656a8decbf7385f6269fbc01186f105d5ac0196
fariyonychka <yustyna1409a@gmail.com>
2025-11-21 12:10:34 +0200

    CI-77 Added additional tags for eks subnets

--- environment-setup/modules/eks/eks-subnets.tf
+++ environment-setup/modules/eks/eks-subnets.tf
@@ -3,8 +3,9 @@ resource "aws_subnet" "private-eks-subnet-a" {
   cidr_block        = var.private-eks-subnet-a
   availability_zone = var.az-a
   tags = merge(var.common_tags_illuminati, {
-    "Name"                                     = "private-eks-subnet-a-${var.env}-${var.cluster-name}"
-    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+    "Name"                                                 = "private-eks-subnet-a-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/${var.cluster-name}-${var.env}" = "owned"
+    "kubernetes.io/role/internal-elb"                      = "1"
   })
 }
 
@@ -13,8 +14,9 @@ resource "aws_subnet" "private-eks-subnet-b" {
   cidr_block        = var.private-eks-subnet-b
   availability_zone = var.az-b
   tags = merge(var.common_tags_illuminati, {
-    "Name"                                     = "private-eks-subnet-b-${var.env}-${var.cluster-name}"
-    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+    "Name"                                                 = "private-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/${var.cluster-name}-${var.env}" = "owned"
+    "kubernetes.io/role/internal-elb"                      = "1"
   })
 }
 
@@ -24,8 +26,9 @@ resource "aws_subnet" "public-eks-subnet-a" {
   availability_zone       = var.az-a
   map_public_ip_on_launch = true
   tags = merge(var.common_tags_illuminati, {
-    "Name"                                     = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
-    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+    "Name"                                                 = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/${var.cluster-name}-${var.env}" = "owned"
+    "kubernetes.io/role/elb"                               = "1"
   })
 }
 
@@ -35,7 +38,8 @@ resource "aws_subnet" "public-eks-subnet-b" {
   availability_zone       = var.az-b
   map_public_ip_on_launch = true
   tags = merge(var.common_tags_illuminati, {
-    "Name"                                     = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
-    "kubernetes.io/cluster/Illuminati-cluster" = "owned"
+    "Name"                                                 = "public-eks-subnet-b-${var.env}-${var.cluster-name}"
+    "kubernetes.io/cluster/${var.cluster-name}-${var.env}" = "owned"
+    "kubernetes.io/role/elb"                               = "1"
   })
 }
