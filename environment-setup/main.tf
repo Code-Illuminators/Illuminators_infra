c42f6793974e2d22c9da080be95c733b0ec8464d
RostyslavHnatyshyn <rostykgnatyshyn@gmail.com>
2025-11-28 12:54:24 +0200

    delete unnecessary data sources, fix some backends issues, change README

--- environment-setup/main.tf
+++ environment-setup/main.tf
@@ -16,13 +16,3 @@ terraform {
   }
 
 }
-
-
-data "terraform_remote_state" "account-vpc" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-state-birdwatching-2025"
-    key    = "env:/${var.env}/account-setup/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
